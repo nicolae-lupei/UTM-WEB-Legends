56c64164bc1f627ce3206e6cd3f614d8dbcaf07e
Lupei Nicolae <nicolae.lupei@indrivo.com>
2019-12-14 20:36:35 +0200

    feat: add users and role management

---
+++ UTM/Controllers/RoleController.cs
@@ -0,0 +1,35 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using UTM.BLL;
+using UTM.DLL;
+
+namespace UTM.Presentation.Controllers
+{
+    public class RoleController : Controller
+    {
+        private readonly RoleManager<Role> _roleManager;
+
+        public RoleController(RoleManager<Role> roleManager)
+        {
+            _roleManager = roleManager;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [Route("api/[controller]/[action]")]
+        [Produces("application/json", Type = typeof(ResultModel))]
+        public async Task<JsonResult> AddNewRole(Role model)
+        {
+            var result = await _roleManager.CreateAsync(model);
+            return Json(new ResultModel
+            {
+                IsSuccess = result.Succeeded
+            });
+        }
+    }
+}
---
+++ UTM/Controllers/UserController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using UTM.DLL.Models;
+
+namespace UTM.Presentation.Controllers
+{
+    public class UserController : Controller
+    {
+        private readonly UserManager<User> _userManager;
+
+        public UserController(UserManager<User> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
